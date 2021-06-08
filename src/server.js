af12ee481fc40df6638658760c976fbe25c67228
Dan Dukhwan Kim <pluvios@pluvios.com>
2021-06-08 23:40:29 +0900

    folders rearaanged

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import express from "express";
 
+const PORT = 3000;
+
 const app = express();
 
-const handleListening = () => console.log("Server listening on port 4000 🩸!!");
+const handleListening = () =>
+  console.log(`Server listening on http://localhost: ${PORT}🩸`);
 
 app.listen(4000, handleListening);
